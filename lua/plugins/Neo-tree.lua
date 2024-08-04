2c155048ffd91e4790c2f7ad89bc515a1ab3186f
Ting Kai Chiu <tingkai.chiu@gmail.com>
2024-08-04 15:22:32 +0800

    Regular Update

--- lua/plugins/Neo-tree.lua
+++ lua/plugins/Neo-tree.lua
@@ -5,6 +5,18 @@ return {
       filesystem = {
         hijack_netrw_behavior = "open_default", -- Hijack netrw and open files directly
         use_libuv_file_watcher = true,
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            -- '.git',
+            -- '.DS_Store',
+            -- 'thumbs.db',
+          },
+          never_show = {},
+        },
       },
       window = {
         position = "current",
